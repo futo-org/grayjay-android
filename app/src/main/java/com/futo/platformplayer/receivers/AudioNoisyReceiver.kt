b75217f789634335dc4ce8fa239ff10b24186825
Koen <koen@pop-os.localdomain>
2024-01-19 17:02:24 +0100

    Possible fix for AudioNoisyReceiver popping up 'App is not responding'.

--- app/src/main/java/com/futo/platformplayer/receivers/AudioNoisyReceiver.kt
+++ app/src/main/java/com/futo/platformplayer/receivers/AudioNoisyReceiver.kt
@@ -4,13 +4,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.futo.platformplayer.logging.Logger
+import com.futo.platformplayer.states.StateApp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 
 class AudioNoisyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        Logger.i(TAG, "Audio Noisy received");
-        MediaControlReceiver.onPauseReceived.emit();
+        StateApp.instance.scopeOrNull?.launch(Dispatchers.Main) {
+            Logger.i(TAG, "Audio Noisy received");
+            MediaControlReceiver.onPauseReceived.emit();
+        }
     }
 
     companion object {
