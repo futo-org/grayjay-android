be2ae096ee11926eaccd8288304c8e1af2c3483f
Kelvin K <kelvin@futo.org>
2024-06-14 13:22:58 +0200

    Fix locked content deserializer

--- app/src/main/java/com/futo/platformplayer/serializers/PlatformContentSerializer.kt
+++ app/src/main/java/com/futo/platformplayer/serializers/PlatformContentSerializer.kt
@@ -2,6 +2,7 @@ package com.futo.platformplayer.serializers
 
 import com.futo.platformplayer.api.media.models.contents.ContentType
 import com.futo.platformplayer.api.media.models.video.SerializedPlatformContent
+import com.futo.platformplayer.api.media.models.video.SerializedPlatformLockedContent
 import com.futo.platformplayer.api.media.models.video.SerializedPlatformNestedContent
 import com.futo.platformplayer.api.media.models.video.SerializedPlatformPost
 import com.futo.platformplayer.api.media.models.video.SerializedPlatformVideo
@@ -30,6 +31,7 @@ class PlatformContentSerializer : JsonContentPolymorphicSerializer<SerializedPla
                 "NESTED_VIDEO" -> SerializedPlatformNestedContent.serializer();
                 "ARTICLE" -> throw NotImplementedError("Articles not yet implemented");
                 "POST" -> SerializedPlatformPost.serializer();
+                "LOCKED" -> SerializedPlatformLockedContent.serializer();
                 else -> throw NotImplementedError("Unknown Content Type Value: ${obj?.jsonPrimitive?.contentOrNull}")
             };
         } else {
@@ -38,6 +40,7 @@ class PlatformContentSerializer : JsonContentPolymorphicSerializer<SerializedPla
                 ContentType.NESTED_VIDEO.value -> SerializedPlatformNestedContent.serializer();
                 ContentType.ARTICLE.value -> throw NotImplementedError("Articles not yet implemented");
                 ContentType.POST.value -> SerializedPlatformPost.serializer();
+                ContentType.LOCKED.value -> SerializedPlatformLockedContent.serializer();
                 else -> throw NotImplementedError("Unknown Content Type Value: ${obj.jsonPrimitive.int}")
             };
         }
