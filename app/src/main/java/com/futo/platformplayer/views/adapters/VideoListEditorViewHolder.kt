11992af81bb0a1e811bb39c0e9ab5bed24c6b2de
Kelvin K <kelvin@futo.org>
2025-06-18 14:27:20 +0200

    Hide duration if unknown

--- app/src/main/java/com/futo/platformplayer/views/adapters/VideoListEditorViewHolder.kt
+++ app/src/main/java/com/futo/platformplayer/views/adapters/VideoListEditorViewHolder.kt
@@ -95,7 +95,13 @@ class VideoListEditorViewHolder : ViewHolder {
             .into(_imageThumbnail);
         _textName.text = v.name;
         _textAuthor.text = v.author.name;
-        _textVideoDuration.text = v.duration.toHumanTime(false);
+
+        if(v.duration > 0) {
+            _textVideoDuration.text = v.duration.toHumanTime(false);
+            _textVideoDuration.visibility = View.VISIBLE;
+        }
+        else
+            _textVideoDuration.visibility = View.GONE;
 
         val historyPosition = StateHistory.instance.getHistoryPosition(v.url)
         _timeBar.progress = historyPosition.toFloat() / v.duration.toFloat();
